aca41faa0c240b94f6c6fc24d60896a99c7a459b
JuniinhoPereira <juniinho.pereira1@gmail.com>
2024-03-12 13:01:23 -0300

    update

---
+++ aula24_Number_manipulation_F_strings.py
@@ -0,0 +1,13 @@
+# The round() function returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
+
+# The default number of decimals is 0, meaning that the function will return the nearest integer.
+
+print(round(8 // 3, 2))
+
+score = 0
+height = 1.8
+isWinning = True
+
+# f-String
+
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
