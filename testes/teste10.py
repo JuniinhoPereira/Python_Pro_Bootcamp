6a99361f672230a05fce272cc22a095223ec0999
JuniinhoPereira <juniinho.pereira1@gmail.comm>
2024-05-27 22:45:51 -0300

    exercicio 10

---
+++ testes/teste10.py
@@ -0,0 +1,19 @@
+'''exercise: leap year
+Write a program that works out whether if a given year is a leap year.
+(https://en.wikipedia.org/wiki/Leap_year)'''
+
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
